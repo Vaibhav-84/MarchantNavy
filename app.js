9e86ead6493128a31f14bc8c461a7b3fe788943c
Vaibhav-84 <69572218+Vaibhav-84@users.noreply.github.com>
2020-09-14 00:17:16 +0530

    connected to database

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ const path = require('path');
 const port =80;
 const app=express();
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/Navy', {useNewUrlParser: true});
+
+const contactSchema = new mongoose.Schema({
+    email: String,
+    number: String,
+    address1: String,
+    address2: String,
+    city: String,
+    state: String,
+    zipno: String
+  });
+const Apply = mongoose.model('Apply', contactSchema);
+app.use(express.urlencoded())
+
 app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/', (req, res) => {   
@@ -11,6 +26,16 @@ res.sendFile(path.join(__dirname, '/index.html'));
 app.get('/contact', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'static/', 'contact.html'));
 })
+app.post('/contact.html', (req, res)=>{
+    let myData = new Apply(req.body);
+    myData.save().then(()=>{
+        res.send("this data have been save to db ")
+    }).catch(()=>{
+        res.status(404).send("not send ")
+    })
+    // res.status(200).redirect('/contact');
+})
+
 app.get('/about', (req, res) => {
 res.sendFile(path.resolve(__dirname, 'static/','about.html'));
 })
